c9911806827f2e5839a2e252d023402273764c25
KaterynaTishchenko <111631799+KaterynaTishchenko@users.noreply.github.com>
2024-03-14 10:22:23 +0200

    Update HW15.py

--- HW15.py
+++ HW15.py
@@ -3,7 +3,12 @@ def format_time(seconds):
     hours, remainder = divmod(remainder, 60 * 60)
     minutes, seconds = divmod(remainder, 60)
 
-    days_word = "день" if days == 1 else "днів"
+    if days == 1:
+        days_word = "день"
+    elif 2 <= days <= 4:
+        days_word = "дні"
+    else:
+        days_word = "днів"
 
     formatted_time = "{:d} {}, {:02d}:{:02d}:{:02d}".format(days, days_word, hours, minutes, seconds)
     return formatted_time
