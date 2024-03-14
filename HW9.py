d036316af4c0daa76bf5cdfe8b7f12878db2646c
KaterynaTishchenko <111631799+KaterynaTishchenko@users.noreply.github.com>
2024-03-14 10:16:35 +0200

    Update HW9.py

--- HW9.py
+++ HW9.py
@@ -3,5 +3,6 @@ import random
 random_list = random.sample(range(1, 101), random.randint(3, 10))
 print("Початковий список:", random_list)
 
-new_list = [random_list[0], random_list[2], random_list[1]]
+list_length = len(random_list)
+new_list = [random_list[0], random_list[3], random_list[list_length - 2]]
 print("Новий список:", new_list)
